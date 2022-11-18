d162b9de12803bd69b534844200aaf572fb098b5
Ivan Yakovkin <yakovkinii@gmail.com>
2022-11-18 15:22:07 +0100

    Initial commit

---
+++ tests/test_minimal.py
@@ -0,0 +1,21 @@
+"""
+Minimal test for YaDraw. Nothing too fancy, but notice the absence of boilerplate code.
+The key principle: avoid building the app around GUI. Make it easy to add GUI to already existing app.
+"""
+import logging
+import unittest
+
+from yadraw import logging_config
+
+
+class TestMinimal(unittest.TestCase):
+    def test_minimal(self):
+        logging_config.init(level=logging.DEBUG)
+        logging.debug("Debug message")
+        logging.info("Info message")
+        logging.warning("Warning message")
+        logging.error("Error message")
+
+
+if __name__ == "__main__":
+    unittest.main()
